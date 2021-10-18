a41d7aba3bd49101ef009e7ffb0529fae6580233
Bhavik Shah (bhavsha2) <bhavsha2@cisco.com>
2021-10-18 17:54:17 +0530

    Add files via upload

--- challenge1/vpc/main.tf
+++ challenge1/vpc/main.tf
@@ -1,14 +1,26 @@
+
+locals {
+  ports_in = [
+    0
+  ]
+  ports_out = [
+    0
+  ]
+}
+
 resource "aws_vpc" "ctf-vpc" {
     cidr_block ="10.0.0.0/16"
-    instance_tenancy = "default"
+    #instance_tenancy = "default"
     enable_dns_support = "true"
     enable_dns_hostnames = "true"
     enable_classiclink = "false"
     tags = {
-      Name = "CTF"
+      Name = "CloudCTF"
     }
   
 }
+
+
 
 
 resource "aws_default_network_acl" "default" {
@@ -22,7 +34,34 @@ resource "aws_default_network_acl" "default" {
         to_port = 0
 
     }
-      egress{
+    ingress{
+        cidr_block = "20.20.20.20/32"
+        rule_no = 110
+        action="allow"
+        from_port = 22
+        to_port = 22
+        protocol="tcp"
+    }
+    ingress{
+        cidr_block = "40.40.40.40/32"
+        rule_no = 120
+        action="allow"
+        from_port = 443
+        to_port = 443
+        protocol="tcp"
+            
+    }
+    ingress{
+        cidr_block = "106.203.219.180/32"
+        rule_no = 130
+        action = "allow"
+        protocol = "tcp"
+        from_port = 80
+        to_port = 80
+    }
+    
+
+    egress{
         protocol = -1
         rule_no = 100
         action = "allow"
@@ -31,27 +70,29 @@ resource "aws_default_network_acl" "default" {
         to_port = 0
         
     }
+    
+      
 }
-#Subnets 
+#Subnets This is a public subnet 
 resource "aws_subnet" "my-public" {
     vpc_id = aws_vpc.ctf-vpc.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "ap-south-1a"
     tags = {
-        Name = "main-public-1"
+        Name = "ctf-public-1"
     }
 
   
 }
-
+# Subnet This is a private subnet
 resource "aws_subnet" "my-private" {
     vpc_id = aws_vpc.ctf-vpc.id
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "false"
     availability_zone = "ap-south-1a"
     tags = {
-      Name = "main-private-1"
+      Name = "ctf-private-1"
     }
 
   
@@ -61,12 +102,12 @@ resource "aws_subnet" "my-private" {
 resource "aws_internet_gateway" "main-igw" {
     vpc_id = aws_vpc.ctf-vpc.id
     tags = {
-        Name = "InternetGW"
+        Name = "CTFInternetGW"
     }
   
 }
 
-#Route table
+#Route table This basically calls the Internet gateway 
 resource "aws_route_table" "main-public" {
     vpc_id = aws_vpc.ctf-vpc.id
     route {
@@ -75,11 +116,11 @@ resource "aws_route_table" "main-public" {
 
     }
     tags = {
-        Name = "main-public-internet"
+        Name = "ctf-public-internet"
     }
 }
 
-#Route association 
+#Route association This basically associates the rouute to internet gateway i...e setting default route for the internet 
 resource "aws_route_table_association" "main-public-1-a" {
     subnet_id = aws_subnet.my-public.id
     route_table_id = aws_route_table.main-public.id
@@ -120,15 +161,49 @@ resource "aws_route_table_association" "main-private-1-a" {
     route_table_id = aws_route_table.main-private.id
 }
 
-#AWS_route table
-resource "aws_route" "public" {
-    route_table_id = aws_route_table.main-public.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main-igw.id
-  
+
+
+resource "aws_security_group" "vpc_security_group"{
+    name = "ctf_security_group"
+    vpc_id = aws_vpc.ctf-vpc.id
+    dynamic "ingress"{
+        for_each = toset(local.ports_in)
+        content{
+
+        description = "inbound to Ec2"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+
+
+    }
+
+    dynamic "egress"{
+        for_each = toset(local.ports_out)
+        content{
+
+        
+        description = "inbound to Ec2"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+
+    }
+
 }
+
 
 output "vpc_id" {
     description = "vpc id"
     value = aws_vpc.ctf-vpc.id
 }
+
+output "public_ip"{
+    value= aws_subnet.my-public.map_public_ip_on_launch
+
+}
+
