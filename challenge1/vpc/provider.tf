e3813f03eace44247df1b132d8ddead361014b64
Bhavik Shah (bhavsha2) <bhavsha2@cisco.com>
2021-10-24 22:36:29 +0530

    Update provider.tf

--- challenge1/vpc/provider.tf
+++ challenge1/vpc/provider.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = var.AWS_REGION
 }
 
-terraform {
+/*
+  terraform {
   backend "s3" {
     bucket = "terraform-infra-state-ctf"
     key    = "home/terraform.tfstate"
     region = "ap-south-1"
   }
 }
+*/ This is the s3 bucket which stores the terraform state on te aws console. This would not be required for the challege deployment stage
