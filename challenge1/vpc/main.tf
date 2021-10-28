324ad624bde0758d835875cfdeb6c8927b6f836b
bhavik <bhavsha2@cisco.com>
2021-10-28 19:39:09 +0530

    modified vpc and provider files

--- challenge1/vpc/main.tf
+++ challenge1/vpc/main.tf
@@ -8,202 +8,202 @@ locals {
   ]
 }
 
-resource "aws_vpc" "ctf-vpc" {
-  cidr_block = "10.0.0.0/16"
-  #instance_tenancy = "default"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
-  tags = {
-    Name = "CloudCTF"
-  }
-
+resource "aws_vpc" "ctf_challenge1_vpc" {
+    cidr_block ="10.0.0.0/16"
+    #instance_tenancy = "default"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "true"
+    enable_classiclink = "false"
+    tags = {
+      Name = "ctf_challenge1_vpc"
+    }
+  
 }
 
 
 
 
 resource "aws_default_network_acl" "default" {
-  default_network_acl_id = aws_vpc.ctf-vpc.default_network_acl_id
-  ingress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
+    default_network_acl_id = aws_vpc.ctf_challenge1_vpc.default_network_acl_id
+    ingress{
+        protocol = -1
+        rule_no = 100
+        action = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port = 0
+        to_port = 0
 
-  }
-  ingress {
-    cidr_block = "20.20.20.20/32"
-    rule_no    = 110
-    action     = "allow"
-    from_port  = 22
-    to_port    = 22
-    protocol   = "tcp"
-  }
-  ingress {
-    cidr_block = "40.40.40.40/32"
-    rule_no    = 120
-    action     = "allow"
-    from_port  = 443
-    to_port    = 443
-    protocol   = "tcp"
+    }
+    ingress{
+        cidr_block = "20.20.20.20/32"
+        rule_no = 110
+        action="allow"
+        from_port = 22
+        to_port = 22
+        protocol="tcp"
+    }
+    ingress{
+        cidr_block = "40.40.40.40/32"
+        rule_no = 120
+        action="allow"
+        from_port = 443
+        to_port = 443
+        protocol="tcp"
+            
+    }
+    ingress{
+        cidr_block = "106.203.219.180/32"
+        rule_no = 130
+        action = "allow"
+        protocol = "tcp"
+        from_port = 80
+        to_port = 80
+    }
+    
 
-  }
-  ingress {
-    cidr_block = "106.203.219.180/32"
-    rule_no    = 130
-    action     = "allow"
-    protocol   = "tcp"
-    from_port  = 80
-    to_port    = 80
-  }
-
-
-  egress {
-    protocol   = -1
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-
-  }
-
-
+    egress{
+        protocol = -1
+        rule_no = 100
+        action = "allow"
+        cidr_block = "0.0.0.0/0"
+        from_port = 0
+        to_port = 0
+        
+    }
+    
+      
 }
 #Subnets This is a public subnet 
 resource "aws_subnet" "my-public" {
-  vpc_id                  = aws_vpc.ctf-vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "ap-south-1a"
-  tags = {
-    Name = "ctf-public-1"
-  }
+    vpc_id = aws_vpc.ctf_challenge1_vpc.id
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "ap-south-1a"
+    tags = {
+        Name = "ctf-public-1"
+    }
 
-
+  
 }
 # Subnet This is a private subnet
 resource "aws_subnet" "my-private" {
-  vpc_id                  = aws_vpc.ctf-vpc.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "ap-south-1a"
-  tags = {
-    Name = "ctf-private-1"
-  }
+    vpc_id = aws_vpc.ctf_challenge1_vpc.id
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = "false"
+    availability_zone = "ap-south-1a"
+    tags = {
+      Name = "ctf-private-1"
+    }
 
-
+  
 }
 
 #Internet GW
 resource "aws_internet_gateway" "main-igw" {
-  vpc_id = aws_vpc.ctf-vpc.id
-  tags = {
-    Name = "CTFInternetGW"
-  }
-
+    vpc_id = aws_vpc.ctf_challenge1_vpc.id
+    tags = {
+        Name = "CTFInternetGW"
+    }
+  
 }
 
 #Route table This basically calls the Internet gateway 
 resource "aws_route_table" "main-public" {
-  vpc_id = aws_vpc.ctf-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main-igw.id
+    vpc_id = aws_vpc.ctf_challenge1_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main-igw.id
 
-  }
-  tags = {
-    Name = "ctf-public-internet"
-  }
+    }
+    tags = {
+        Name = "ctf-public-internet"
+    }
 }
 
 #Route association This basically associates the rouute to internet gateway i...e setting default route for the internet 
 resource "aws_route_table_association" "main-public-1-a" {
-  subnet_id      = aws_subnet.my-public.id
-  route_table_id = aws_route_table.main-public.id
-
+    subnet_id = aws_subnet.my-public.id
+    route_table_id = aws_route_table.main-public.id
+  
 }
 
 resource "aws_eip" "nat" {
-  vpc = true
-
+    vpc = true
+  
 }
 
 #NAt GAteway 
 
 resource "aws_nat_gateway" "nat-gw" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.my-public.id
-  depends_on    = [aws_internet_gateway.main-igw]
-
+    allocation_id = aws_eip.nat.id
+    subnet_id = aws_subnet.my-public.id
+    depends_on = [aws_internet_gateway.main-igw]
+  
 }
 
 #VPC Setup for NAT
 resource "aws_route_table" "main-private" {
-  vpc_id = aws_vpc.ctf-vpc.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat-gw.id
+    vpc_id = aws_vpc.ctf_challenge1_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat-gw.id
 
-  }
-  tags = {
-    Name = "nat-gw-private"
-  }
-
+    }
+    tags = {
+        Name = "nat-gw-private"
+    }
+  
 }
 
 #Route association private 
 resource "aws_route_table_association" "main-private-1-a" {
-  subnet_id      = aws_subnet.my-private.id
-  route_table_id = aws_route_table.main-private.id
+    subnet_id = aws_subnet.my-private.id
+    route_table_id = aws_route_table.main-private.id
 }
 
 
 
-resource "aws_security_group" "vpc_security_group" {
-  name   = "ctf_security_group"
-  vpc_id = aws_vpc.ctf-vpc.id
-  dynamic "ingress" {
-    for_each = toset(local.ports_in)
-    content {
+resource "aws_security_group" "vpc_security_group"{
+    name = "ctf_security_group"
+    vpc_id = aws_vpc.ctf_challenge1_vpc.id
+    dynamic "ingress"{
+        for_each = toset(local.ports_in)
+        content{
 
-      description = "inbound to Ec2"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+        description = "inbound to Ec2"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+
+
     }
 
+    dynamic "egress"{
+        for_each = toset(local.ports_out)
+        content{
 
-  }
+        
+        description = "inbound to Ec2"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
 
-  dynamic "egress" {
-    for_each = toset(local.ports_out)
-    content {
-
-
-      description = "inbound to Ec2"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
     }
-
-  }
 
 }
 
 
 output "vpc_id" {
-  description = "vpc id"
-  value       = aws_vpc.ctf-vpc.id
+    description = "vpc id"
+    value = aws_vpc.ctf_challenge1_vpc.id
 }
 
-output "public_ip" {
-  value = aws_subnet.my-public.map_public_ip_on_launch
+output "public_ip"{
+    value= aws_subnet.my-public.map_public_ip_on_launch
 
 }
 
--- challenge1/vpc/provider.tf
+++ challenge1/vpc/provider.tf
@@ -2,12 +2,10 @@ provider "aws" {
   region = var.AWS_REGION
 }
 
-/*
-  terraform {
-  backend "s3" {
-    bucket = "terraform-infra-state-ctf"
-    key    = "home/terraform.tfstate"
-    region = "ap-south-1"
-  }
+terraform {
+    backend "s3"{
+        bucket="terraform-infra-state-ctf"
+        key ="home/terraform.tfstate"
+        region="ap-south-1"
+    }
 }
-*/ This is the s3 bucket which stores the terraform state on te aws console. This would not be required for the challege deployment stage
