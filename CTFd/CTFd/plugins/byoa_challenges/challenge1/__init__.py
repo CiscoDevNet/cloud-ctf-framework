b91141eb7c601bf92f4fef6aa0a257c9006a9ebd
John Groetzinger <jgroetzi@cisco.com>
2021-11-08 13:00:45 -0500

    challenge1 example validaiton

---
+++ CTFd/CTFd/plugins/byoa_challenges/challenge1/__init__.py
@@ -0,0 +1,6 @@
+def validate_chalenge():
+    # TODO Bhavik to fill in validation code
+    #example bad return
+    return {"validate_result": False, "errors": ["could not validate"]}
+    #example good return
+    return {"validate_result": False, "flag": "thisistheflag"}
