7462a0883bddc74c5e9b2f8785090367f5360015
John Groetzinger <jgroetzi@cisco.com>
2021-11-11 20:38:30 -0500

    removed import because python is stupid

--- CTFd/CTFd/plugins/byoa_challenges/challenge1/__init__.py
+++ CTFd/CTFd/plugins/byoa_challenges/challenge1/__init__.py
@@ -1,9 +1,10 @@
-from .. import ByoaChallengeDeploys
+# from .. import ByoaChallengeDeploys
 
 
-def validate_chalenge(bcd: ByoaChallengeDeploys):
+def validate_chalenge(bcd):
     # TODO Bhavik to fill in validation code
     tf_data = bcd.get_terraform_state_dict()
+    return tf_data
 
     #example bad return
     return {"validate_result": False, "errors": ["could not validate"]}
