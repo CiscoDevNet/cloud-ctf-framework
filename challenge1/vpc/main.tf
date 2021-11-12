936d2c88cc4f0a74c0026f71bec3e3ef36bd1635
bhavik <bhavsha2@cisco.com>
2021-11-12 21:40:15 +0530

    modified the vars.tf

--- challenge1/vpc/main.tf
+++ challenge1/vpc/main.tf
@@ -82,7 +82,7 @@ resource "aws_subnet" "my-public" {
     vpc_id = aws_vpc.ctf_challenge1_vpc.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    #availability_zone = "ap-south-1a"
+    availability_zone = lookup(var.availability_zone , var.AWS_REGION)
     tags = {
         Name = "ctf-public-1"
     }
@@ -94,7 +94,7 @@ resource "aws_subnet" "my-private" {
     vpc_id = aws_vpc.ctf_challenge1_vpc.id
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "false"
-    #availability_zone = "ap-south-1a"
+    availability_zone = lookup(var.availability_zone , var.AWS_REGION)
     tags = {
       Name = "ctf-private-1"
     }
