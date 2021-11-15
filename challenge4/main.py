d4716d4c3b2c3c837c643d166fbaa4a8de7af1b3
Ankush Kumar <ankushk2@cisco.com>
2021-11-15 20:43:24 +0530

    Added chall4

---
+++ challenge4/main.py
@@ -0,0 +1,52 @@
+import requests
+import uuid
+import logging
+from flask import Flask, request, render_template, jsonify
+from flask_sessionstore import Session
+from flask_session_captcha import FlaskSessionCaptcha
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Database Config
+# If your mongodb runs on a different port
+# change 27017 to that port number
+mongoClient = MongoClient('localhost', 27017)
+
+# Captcha Configuration
+app.config["SECRET_KEY"] = uuid.uuid4()
+app.config['CAPTCHA_ENABLE'] = True
+
+# Set 5 as character length in captcha
+app.config['CAPTCHA_LENGTH'] = 5
+
+# Set the captcha height and width
+app.config['CAPTCHA_WIDTH'] = 160
+app.config['CAPTCHA_HEIGHT'] = 60
+app.config['SESSION_MONGODB'] = mongoClient
+app.config['SESSION_TYPE'] = 'mongodb'
+
+# Enables server session
+Session(app)
+
+# Initialize FlaskSessionCaptcha
+captcha = FlaskSessionCaptcha(app)
+
+@app.route('/',methods = ['POST', 'GET'])
+def url():
+   if request.method == 'POST':
+      if captcha.validate():
+          user = request.form['url']
+          print(user)
+          try:
+              resp = requests.get(user)
+              return render_template("index.html",abc=resp.text)
+          except Exception as e:
+              return render_template('excp.html')
+      else:
+          return render_template('capta.html')
+   else:
+      return render_template('form.html')
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0',debug = True)
