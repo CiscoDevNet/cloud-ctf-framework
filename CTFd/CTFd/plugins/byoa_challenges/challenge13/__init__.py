a617fd8c05826b6373901d795bc175a2dd780ceb
John Groetzinger <jgroetzi@cisco.com>
2021-11-16 17:56:11 -0500

    bug fix chal13 when outputs don't have bucket name

--- CTFd/CTFd/plugins/byoa_challenges/challenge13/__init__.py
+++ CTFd/CTFd/plugins/byoa_challenges/challenge13/__init__.py
@@ -14,7 +14,17 @@ class ByoaChallengeValidationReturn:
 def validate_chalenge(bcd):
 
     tf_data = bcd.get_terraform_state_dict()
-    bucket_name = tf_data['outputs']['bucket_name']['value']
+    bucket_name = None
+    if 'bucket_name' in tf_data['outputs'] and 'value' in tf_data['outputs']['bucket_name']:
+        bucket_name = tf_data['outputs']['bucket_name']['value']
+    elif len(tf_data['resources']):
+        for res in tf_data['resources']:
+            if 'type' in res and 'name' in res and res['type'] == 'aws_s3_bucket':
+                bucket_name = res['name']
+                break
+
+    if bucket_name is None:
+        return ByoaChallengeValidationReturn(message="Hit an error trying to validate, unable to find bucket name. please reach out to site admins for assistance.", result=False)
 
     s3 = boto3.client('s3', aws_access_key_id=bcd.get_byoa_team_aws_info().AWS_ACCESS_KEY_ID, aws_secret_access_key=bcd.get_byoa_team_aws_info().AWS_SECRET_ACCESS_KEY, region_name=bcd.get_byoa_team_aws_info().AWS_REGION)
     to_check = {"BlockPublicAcls": True,"IgnorePublicAcls": True,"BlockPublicPolicy": True,"RestrictPublicBuckets": True}
